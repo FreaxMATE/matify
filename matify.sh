f00b2ff817dd76e3df361ff3163fbf5593a5bf11
Konstantin Unruh <freaxmate@protonmail.com>
2021-03-05 21:55:32 +0100

    expand scrip

--- matify.sh
+++ matify.sh
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-remove_packages="python-pip imagewriter thunderbird timeshift bauh lollypop steam-manjaro snapd mate-tweak"
+remove_packages="matcha-gtk-theme papirus-icon-theme papirus-maia-icon-theme python-pip imagewriter thunderbird timeshift bauh lollypop steam-manjaro snapd mate-tweak"
 
 install_packages="gcc redshift git gnome-passwordsafe gnome-podcasts gnome-boxes glade libreoffice-still devhelp audacity celluloid easytag tuxguitar rhythmbox evolution simple-scan"
 
@@ -23,7 +23,7 @@ echo "Removing Packages: "
 echo "  " $remove_packages
 echo
 for package in $remove_packages; do
-    sudo pacman -Rsn $package
+    sudo pamac remove $install_packages
 done
 
 echo "Installing Packages: "
@@ -33,22 +33,26 @@ echo
 sudo pamac install $install_packages
 
 cd /home/$USER/
-mkdir src
-cd /home/$USER/src
+mkdir src && cd /home/$USER/src
 # install opt software
+git clone https://github.com/FreaxMATE/mate-layouts.git
+git clone https://github.com/FreaxMATE/manjaro-mate-settings.git
+git clone https://github.com/FreaxMATE/papirus-folders.git
+git clone https://github.com/FreaxMATE/Matcha-gtk-theme.git
+git clone https://github.com/FreaxMATE/papirus-icon-theme.git
 
-git clone https://github.com/FreaxMATE/mate-layouts
+# install mate-layouts
 cd mate-layouts
 sudo pip3 install .
 mate-layouts --layout default
 
+# set menu icon to default Papirus icon
 cd /home/$USER/.icons/
 mv logo-manjaro.svg logo-manjaro-legacy.svg
-cd /home/$USER/src
-git clone https://github.com/FreaxMATE/manjaro-mate-settings
-cd manjaro-mate-settings
+cd /home/$USER/src/manjaro-mate-settings
 mv logo-manjaro.svg /home/$USER/.icons/
 
+# install slideshow wallpaper
 sudo mv mountains-mate-breakfast.jpg /usr/share/backgrounds/mountains-mate-breakfast.jpg
 sudo mv mountains-mate-morning.jpg /usr/share/backgrounds/mountains-mate-morning.jpg
 sudo mv mountains-mate-noon.jpg /usr/share/backgrounds/mountains-mate-noon.jpg
@@ -58,8 +62,33 @@ sudo mv mountains-mate-night.jpg /usr/share/backgrounds/mountains-mate-night.jpg
 sudo mv mountains-mate.xml /usr/share/backgrounds/mountains-mate.xml
 gsettings set org.mate.background picture-filename /usr/share/backgrounds/mountains-mate.xml
 
-
+# start redshift automatically
 sudo mv redshift-gtk.desktop /home/$USER/.config/autostart/redshift-gtk.desktop
 
+# install Matcha theme
+cd /home/$USER/src/Matcha-gtk-theme
+git checkout green
+sudo ./install.sh
+
+# install Papirus icon theme
+cd /home/$USER/src/papirus-icon-theme
+sudo make install
+
+# install papirus folders
+cd /home/$USER/src/papirus-folders
+sudo make install
+papirus-folders -t Papirus-Dark  -C mategreen
+
+# theme, animations
 gsettings set org.mate.interface enable-animations false
+gsettings set org.mate.interface gtk-theme Matcha-pueril
+gsettings set org.mate.interface icon-theme Papirus
+
+gsettings set org.mate.caja.desktop computer-icon-visible false
+gsettings set org.mate.caja.desktop home-icon-visible false
+gsettings set org.mate.caja.desktop trash-icon-visible false
+gsettings set org.mate.caja.desktop volumes-visible false
+
+# bash
+echo "export HISTCONTROL=ignoreboth:erasedups" >> .bashrc
 
